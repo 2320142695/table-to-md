ad0d58a931d9a2c9839aee0a9c36dcc4d2bd2ea8
cxx <cxx2320@foxmail.com>
2021-01-28 15:09:14 +0800

    未找到元素跳出

--- content_script.js
+++ content_script.js
@@ -1,7 +1,12 @@
-const rows = document.querySelector('.scrollable table').rows;
-let md = `|列|类型|注释|\n|-|-|-|\n`;
-for (let i = 1; i < rows.length; i++) {    //--循环所有的行
-    const cells = rows[i].cells;
-    md += `|${cells[0].innerHTML.replace(/\s*/g, "")}|${cells[1].textContent}|${cells[2].innerHTML.replace(/\s*/g, "")}|\n`;
-}
-console.log(md);
+(function () {
+    if(!document.querySelector('.scrollable table')){
+        return;
+    }
+    const rows = document.querySelector('.scrollable table').rows;
+    let md = `|列|类型|注释|\n|-|-|-|\n`;
+    for (let i = 1; i < rows.length; i++) {    //--循环所有的行
+        const cells = rows[i].cells;
+        md += `|${cells[0].innerHTML.replace(/\s*/g, "")}|${cells[1].textContent}|${cells[2].innerHTML.replace(/\s*/g, "")}|\n`;
+    }
+    console.log(md);
+})()
