3f80c5b021ccb98f952bdcc3c61da9207a5f2d03
cxx <cxx2320@foxmail.com>
2021-01-28 14:49:42 +0800

    init

---
+++ content_script.js
@@ -0,0 +1,7 @@
+const rows = document.querySelector('.scrollable table').rows;
+let md = `|列|类型|注释|\n|-|-|-|\n`;
+for (let i = 1; i < rows.length; i++) {    //--循环所有的行
+    const cells = rows[i].cells;
+    md += `|${cells[0].innerHTML.replace(/\s*/g, "")}|${cells[1].textContent}|${cells[2].innerHTML.replace(/\s*/g, "")}|\n`;
+}
+console.log(md);
