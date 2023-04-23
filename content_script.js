82ea6d9f4a97a836f8bfdf53f478cf913d3cf214
YueLiang <34790068+cxx2320@users.noreply.github.com>
2023-04-23 10:04:40 +0800

    新增两种格式

--- content_script.js
+++ content_script.js
@@ -9,4 +9,25 @@
         md += `|${cells[0].innerHTML.replace(/\s*/g, "")}|${cells[1].textContent}|${cells[2].innerHTML.replace(/\s*/g, "")}|\n`;
     }
     console.log(md);
+
+    md = `|列|注释|\n|-|-|\n`;
+    for (let i = 1; i < rows.length; i++) {    //--循环所有的行
+        const cells = rows[i].cells;
+        md += `|${cells[0].innerHTML.replace(/\s*/g, "")}|${cells[2].innerHTML.replace(/\s*/g, "")}|\n`;
+    }
+    console.log(md);
+
+    md = `|列|注释|列|注释|\n|-|-|-|-|\n`;
+    for (let i = 1; i < rows.length;) {    //--循环所有的行
+        const cells = rows[i].cells;
+        md += `|${cells[0].innerHTML.replace(/\s*/g, "")}|${cells[2].innerHTML.replace(/\s*/g, "")}|`;
+        if (rows[i + 1] !== undefined) {
+            let cells2 = rows[i + 1].cells;
+            md += `${cells2[0].innerHTML.replace(/\s*/g, "")}|${cells2[2].innerHTML.replace(/\s*/g, "")}|\n`;
+        } else {
+            md += `||\n`;
+        }
+        i = i + 2;
+    }
+    console.log(md);
 })()
